5d20f2040da15164a724ef1767096bad5068e95b
Bibek Ghimire <gbibek34@gmail.com>
2025-04-20 23:52:16 -0400

    Latest Pull

--- server/utils/authHandler.js
+++ server/utils/authHandler.js
@@ -1,46 +1,44 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-
-
-const getToken = (user) =>{
+const getToken = (user) => {
     return jwt.sign(
-    {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin
-    },
-    process.env.JWT_SECRET,
-    {expiresIn: '48h'})
+        {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '48h' })
 }
 
-const isAuth = async(req, res, next)=>{
+const isAuth = async (req, res, next) => {
     const token = req.headers.authorization;
 
-    if (token){
+    if (token) {
         const onlyToken = token.slice(7, token.length)
-        jwt.verify(onlyToken, process.env.JWT_SECRET, (err, decode) =>{
+        jwt.verify(onlyToken, process.env.JWT_SECRET, (err, decode) => {
             if (err) {
-                return res.status(401).send({message: "Invalid Token"});
+                return res.status(401).send({ message: "Invalid Token" });
             }
             req.user = decode;
             next();
             return;
         });
     } else {
-        return res.status(401).send({message: 'Token is not supplied.'})
+        return res.status(401).send({ message: 'Token is not supplied.' })
     }
 }
 
-const isAdmin = async(req, res, next) => {
+const isAdmin = async (req, res, next) => {
     console.log(req.user)
 
-    if (req.user && req.user.isAdmin){
+    if (req.user && req.user.isAdmin) {
         return next()
     }
-    return res.status(401).send({message: "Admin token is not valid."})
+    return res.status(401).send({ message: "Admin token is not valid." })
 }
 
-module.exports = {getToken, isAuth, isAdmin}
+module.exports = { getToken, isAuth, isAdmin }
 
