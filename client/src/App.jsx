2712c40af9798ea9dedd7f988b960b2248f92cbd
Bibek Ghimire <gbibek34@gmail.com>
2025-05-22 22:39:36 -0400

    Changes to the base client structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import LoginPage from './User/pages/LoginPage'
-import RegisterPage from './User/pages/RegisterPage';
-import AddressPage from './User/pages/AddressPage';
-import ProfilePage from './User/pages/ProfilePage';
+import "./App.css";
+import LoginPage from "./User/pages/LoginPage";
+import RegisterPage from "./User/pages/RegisterPage";
+import AddressPage from "./User/pages/AddressPage";
+import ProfilePage from "./User/pages/ProfilePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LoginPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
-        <Route path='/address' element={<AddressPage/>}/>
-        <Route path='/profile' element={<ProfilePage/>} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/address" element={<AddressPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
-    
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
