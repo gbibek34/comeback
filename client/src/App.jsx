195c4bc28c68f782df6be1253fc91a6b93742ea7
Bibek Ghimire <gbibek34@gmail.com>
2025-05-29 21:51:26 -0400

    Small Changes, Files Creation for Products Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import AddressPage from "./features/user/pages/AddressPage";
 import ProfilePage from "./features/user/pages/ProfilePage";
 import EditProfilePage from "./features/user/pages/EditProfilePage";
 import EditAddressPage from "./features/user/pages/EditAddressPage";
+import AllProductsPage from "./features/products/pages/AllProductsPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,8 +16,10 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/address" element={<AddressPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/editProfile" element={<EditProfilePage/>}/>
-        <Route path="/editAddress" element={<EditAddressPage/>}/>
+        <Route path="/editProfile" element={<EditProfilePage />} />
+        <Route path="/editAddress" element={<EditAddressPage />} />
+        <Route path="/products" element={<AllProductsPage />} />
+        <Route path="/products" element={<AllProductsPage />} />
       </Routes>
     </BrowserRouter>
   );
