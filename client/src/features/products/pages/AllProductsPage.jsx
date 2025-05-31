1c0da5c30d533a1f5062e7f457996c53c9e74eb2
gbibek34 <gbibek34@gmail.com>
2025-05-31 15:27:00 -0400

    Fetch all products in main products page

--- client/src/features/products/pages/AllProductsPage.jsx
+++ client/src/features/products/pages/AllProductsPage.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import SingleProductCard from '../components/SingleProductCard'
+import { API_URL } from '../../../config'
 
-function AllProductsPage() {
+export default function AllProductsPage() {
+    const [allProducts, setAllProducts] = useState([''])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(
+                    `${API_URL}/product/all`
+                )
+                console.log(response.data.data)
+                setAllProducts(response.data.data)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        fetchProducts()
+    }, [])
+
+
     return (
         <div>
-            <SingleProductCard />
+            <SingleProductCard allProducts={allProducts} />
         </div>
     )
 }
-
-export default AllProductsPage
