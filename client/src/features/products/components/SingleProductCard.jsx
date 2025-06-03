05c19b2df7bfcb3e488711bb5e4d9b6dc416f5dc
gbibek34 <gbibek34@gmail.com>
2025-06-03 16:25:49 -0400

    Add products

--- client/src/features/products/components/SingleProductCard.jsx
+++ client/src/features/products/components/SingleProductCard.jsx
@@ -6,12 +6,16 @@ function SingleProductCard({ allProducts }) {
             {
                 allProducts.map((product) => (
                     <div key={product._id}>
+                        <div>***************************************</div>
                         <div>Product Title:{product.title}</div>
                         <div>Product Desc:{product.description}</div>
                         <div>Stock Available:{product.stock}</div>
                         <div>Price:${product.price}</div>
-                        <div>Add To Cart</div>
-                        <div>_____________________________________</div>
+                        <button>Add To Cart</button>
+                        <br />
+                        <button>Edit Product</button>
+                        <button>Delete Product</button>
+                        <div>***************************************</div>
                     </div>
                 ))
             }
